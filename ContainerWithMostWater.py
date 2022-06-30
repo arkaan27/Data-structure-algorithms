6b7796f6c17a1e8cd6af902bdb4c7bc985558d9a
Arkaan Quanunga <arkaan.quanunga@QUALITEST.LOCAL>
2022-06-30 10:17:00 +0100

    Completed problem for finding the maximum area of water trapped in a container from a given array with Brute Force and Optimisation

---
+++ ContainerWithMostWater.py
@@ -0,0 +1,65 @@
+"""
+You are given an array of positive integers where each
+integer represents the height of vertical line
+on a chart. Find two lines which together with the
+x-axis forms a container that would hold the greatest
+amount of water.
+Return the area of water it would hold.
+
+
+Assumptions:
+1. Does the thickness of the lines affect the area?
+    No, assume they take up no space
+2. Do the left and right sides of the graph count as walls?
+    No, the sides cannot be used to form container
+3. Can we pick two values if one value is higher in the middle
+    Yes, the value in the middle won't affect the container
+
+"""
+
+
+def MostWaterBruteForce(A):
+    # Time: O(n^2)
+    # Space: O(1)
+
+    maxArea = 0
+    for x in range(0, len(A)):
+        for y in range(x + 1, len(A)):
+            height = min(A[x], A[y])
+            width = y - x
+            maxArea = max(maxArea, height * width)
+
+    return maxArea
+
+
+def MostWaterOptimised(A):
+    # Time: O(n)
+    # Space: O(1)
+    p1 = 0
+    p2 = len(A) - 1
+    maxArea = 0
+    # Calculate Area
+    while p1 < p2:
+        height = min(A[p1], A[p2])
+        width = p2 - p1
+        maxArea = max(maxArea, height * width)
+        # Logic to move pointer
+        if A[p1] <= A[p2]:
+            p1 += 1
+        else:
+            p2 -= 1
+
+    return maxArea
+
+
+if __name__ == '__main__':
+    tc1 = [7, 1, 2, 3, 9]  # 28
+    tc2 = []  # 0
+    tc3 = [7]  # 0
+    tc4 = [6, 9, 3, 4, 5, 8]  # 32
+    tcs = [tc1, tc2, tc3, tc4]
+    for tc in tcs:
+        print("Brute Force Solutions")
+        print(MostWaterBruteForce(tc))
+        print("Optimised Solutions")
+        print(MostWaterOptimised(tc))
