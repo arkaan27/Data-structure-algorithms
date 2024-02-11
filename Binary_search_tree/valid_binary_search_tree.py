3b65beca32560a580e8fd4b4044e9fd11bfd51c7
Arkaan <arkaankanunga@gmail.com>
2024-02-11 22:29:06 +0000

    starting valid_binary_search_tree.py

---
+++ Binary_search_tree/valid_binary_search_tree.py
@@ -0,0 +1,27 @@
+"""
+
+Given the root of a binary tree, determine if it is a valid binary search tree (BST)
+
+A valid BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+
+Can there be duplicate values in the tree?
+
+Yes, if you receive duplicate values the tree is not a valid binary search tree
+"""
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def is_valid_bst(root: Optional[TreeNode]) -> bool:
+    pass
+
