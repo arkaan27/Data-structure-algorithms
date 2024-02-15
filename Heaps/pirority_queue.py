b32ce0fd570462853fd6c2ace17c320260863e7b
Arkaan <arkaankanunga@gmail.com>
2024-02-15 16:24:19 +0000

    learning about priority queue

---
+++ Heaps/pirority_queue.py
@@ -0,0 +1,40 @@
+import heapq
+
+
+class PriorityQueue:
+    def __init__(self, comparator=lambda a, b: a < b):
+        self.heap = []
+        self.comparator = comparator
+
+    def size(self):
+        return len(self.heap)
+
+    def peek(self):
+        return self.heap[0] if self.heap else None
+
+    def isEmpty(self):
+        return not self.heap
+
+    def push(self, value):
+        # Invert value for max heap, if the comparator is for a max heap
+        value = (value, value) if self.comparator(0, 1) else (-value, value)
+        heapq.heappush(self.heap, value)
+        return self.size()
+
+    def pop(self):
+        if self.heap:
+            value = heapq.heappop(self.heap)
+            return value[1] if self.comparator(0, 1) else -value[0]
+
+
+# Example usage
+pq = PriorityQueue(lambda a, b: a > b)  # Max heap
+pq.push(15)
+pq.push(12)
+pq.push(50)
+pq.push(7)
+pq.push(40)
+pq.push(22)
+
+while not pq.isEmpty():
+    print(pq.pop())
