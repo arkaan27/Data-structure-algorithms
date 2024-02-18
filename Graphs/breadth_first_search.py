44e2b0c635f4bd5c6d76c0af29fbdbfad7bd4070
Arkaan <arkaankanunga@gmail.com>
2024-02-18 15:35:52 +0000

    Adding breadth first search for adjacency matrix

--- Graphs/breadth_first_search.py
+++ Graphs/breadth_first_search.py
@@ -12,8 +12,20 @@ adjacency_list = [
     [2]
 ]
 
+adjacency_matrix = [
+    [0, 1, 0, 1, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0, 1, 0],
+    [0, 0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0, 0]
+]
 
-def traversal_bfs_graph(graph):
+
+def traversal_bfs_adj_list(graph):
     queue = deque([0])
     values = []
     seen = set()
@@ -31,5 +43,25 @@ def traversal_bfs_graph(graph):
     return values
 
 
+def traversal_bfs_adj_matrix(graph):
+    queue = deque([0])
+    values = []
+    seen = set()
+
+    while queue:
+        vertex = queue.popleft()
+
+        if vertex not in seen:
+            values.append(vertex)
+            seen.add(vertex)
+
+            for v in range(len(graph[vertex])):
+                if graph[vertex][v] > 0 and v not in seen:
+                    queue.append(v)
+
+    return values
+
+
 if __name__ == "__main__":
-    print(traversal_bfs_graph(adjacency_list))
+    print(traversal_bfs_adj_list(adjacency_list))
+    print(traversal_bfs_adj_matrix(adjacency_matrix))
