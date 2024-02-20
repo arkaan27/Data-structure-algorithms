eec2a0c917769c7739b693e49edd3dd2b2d65d63
Arkaan <arkaankanunga@gmail.com>
2024-02-20 15:52:15 +0000

    finishing course scheduler whether it can be finished or not using a brute force solution

---
+++ Graphs/course_scheduler.py
@@ -0,0 +1,58 @@
+"""
+
+There are a total of n courses to take, labeled from 0 to n-1
+
+Some courses have prerequisite courses. This is expressed as a pair i.e. [1,0] which indicates you must take course 0
+before taking course 1.
+
+Given the total number of courses and an array of prerequisite pairs, return if it is possible to finish all courses.
+
+Verify the Constraints:
+
+Can we have courses unconnected to other courses?
+Yes account for separate course chains.
+
+"""
+
+
+def can_finish(num_of_courses, prerequisites):
+    adj_list = [[] for _ in range(num_of_courses)]
+
+    for pre in prerequisites:
+        adj_list[pre[1]].append(pre[0])
+
+    def is_cyclic(course, visited, rec_stack):
+        visited[course] = True
+        rec_stack[course] = True
+
+        for neighbor in adj_list[course]:
+            if not visited[neighbor]:
+                if is_cyclic(neighbor, visited, rec_stack):
+                    return True
+            elif rec_stack[neighbor]:
+                return True
+
+        rec_stack[course] = False
+        return False
+
+    visited = [False] * num_of_courses
+    rec_stack = [False] * num_of_courses
+
+    # Check each course for cycles
+    for course in range(num_of_courses):
+        if not visited[course]:
+            if is_cyclic(course, visited, rec_stack):
+                return False
+
+    return True
+
+
+if __name__ == "__main__":
+    n = 6
+    prerequisites = [
+        [1, 0], [2, 1],
+        [2, 5], [0, 3],
+        [4, 3], [3, 5],
+        [4, 5]
+    ]
+    print(can_finish(n, prerequisites))
