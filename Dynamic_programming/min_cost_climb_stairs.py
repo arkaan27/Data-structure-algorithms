9ba2c6594a35574e317a445bc409d886f63b0247
Arkaan <arkaankanunga@gmail.com>
2024-02-21 14:25:56 +0000

    implementing the optimised solution to be O(1)

--- Dynamic_programming/min_cost_climb_stairs.py
+++ Dynamic_programming/min_cost_climb_stairs.py
@@ -43,8 +43,46 @@ def min_cost_dp(i, cost, dp):
     return dp[i]
 
 
+# Bottom up approach for solving
+def min_cost_climbing_stairs_bu(cost):
+    n = len(cost)
+    return min(min_cost_bu(n - 1, cost), min_cost_bu(n - 2, cost))
+
+
+def min_cost_bu(i, cost):
+    if i < 0:
+        return 0
+    if i == 0 or i == 1:
+        return cost[i]
+
+    return cost[i] + min(min_cost_bu(i - 1, cost), min_cost_bu(i - 2, cost))
+
+
+# Optimising the Space Complexity to O(1)
+def min_cost_climbing_stairs_optimised(cost):
+    n = len(cost)
+
+    if n == 0:
+        return 0
+    if n == 1:
+        return cost[0]
+    dp_one = cost[0]
+    dp_two = cost[1]
+    for i in range(2, n):
+        curr = cost[i] + min(dp_one, dp_two)
+        dp_one = dp_two
+        dp_two = curr
+
+    return min(dp_one, dp_two)
+
+
 if __name__ == "__main__":
+    cost = [20, 15, 30, 5]
     start_time_td = time.time()
-    print(min_cost_climbing_stairs_td([20, 15, 30, 5]), f"Completed in {time.time() - start_time_td}")
+    print(min_cost_climbing_stairs_td(cost), f"Completed in {time.time() - start_time_td}")
     start_time_dp = time.time()
-    print(min_cost_climbing_stairs_dp([20, 15, 30, 5]), f"Completed in {time.time() - start_time_dp}")
+    print(min_cost_climbing_stairs_dp(cost), f"Completed in {time.time() - start_time_dp}")
+    start_time_bu = time.time()
+    print(min_cost_climbing_stairs_bu(cost), f"Completed in {time.time() - start_time_bu}")
+    start_time_opti = time.time()
+    print(min_cost_climbing_stairs_optimised(cost), f"Completed in {time.time() - start_time_opti}")
