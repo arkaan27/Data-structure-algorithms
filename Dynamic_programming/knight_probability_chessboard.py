21d1117d12dd8dbdc3d4557f284e958206b631ca
Arkaan <arkaankanunga@gmail.com>
2024-02-21 19:02:07 +0000

    adding bottom up solution with optimised version

--- Dynamic_programming/knight_probability_chessboard.py
+++ Dynamic_programming/knight_probability_chessboard.py
@@ -37,6 +37,7 @@ def knight_p_recurs(n, k, row, column):
 
     return result
 
+
 # Applying dynamic programming to reduce time complexity:
 # Time: O(N^2 x k) Space: O(N^2 x k)
 def knight_p_dp(n, k, row, column):
@@ -61,6 +62,52 @@ def recurse(n, k, row, column, dp):
     return response
 
 
+# Bottom up iterative solution
+
+def knight_p_bu(n, k, row, column):
+    dp = [[[0 for _ in range(n)] for _ in range(n)] for _ in range(k + 1)]
+    dp[0][row][column] = 1
+
+    for step in range(1, k + 1):
+        for r in range(n):
+            for c in range(n):
+                for d in DIRECTIONS:
+                    prev_row, prev_col = r + d[0], c + d[1]
+                    if 0 <= prev_row < n and 0 <= prev_col < n:
+                        dp[step][r][c] += (dp[step - 1][prev_row][prev_col]) / 8
+
+    result = 0
+    for i in range(n):
+        for j in range(n):
+            result += dp[k][i][j]
+    return result
+
+
+# Bottom up optimised solution with just two arrays in space.
+
+def knight_p_optimised(n, k , r, c):
+    prev_dp = [[0 for _ in range(n)] for _ in range(n)]
+    next_dp = [[0 for _ in range(n)] for _ in range(n)]
+    prev_dp[r][c] = 1
+
+    for step in range(1, k + 1):
+        for row in range(n):
+            for col in range(n):
+                for curr_dir in DIRECTIONS:
+                    prev_row = row + curr_dir[0]
+                    prev_col = col + curr_dir[1]
+                    if 0 <= prev_row < n and 0 <= prev_col < n:
+                        next_dp[row][col] += prev_dp[prev_row][prev_col] / 8
+
+        prev_dp, next_dp = next_dp, [[0 for _ in range(n)] for _ in range(n)]
+
+    result = 0
+
+    result += sum(sum(row) for row in prev_dp)
+
+    return result
+
+
 if __name__ == "__main__":
     n = 6
     k = 3
@@ -68,3 +115,5 @@ if __name__ == "__main__":
     column = 2
     print(knight_p_recurs(n, k, row, column))
     print(knight_p_dp(n, k, row, column))
+    print(knight_p_bu(n, k, row, column))
+    print(knight_p_optimised(n, k, row, column))
