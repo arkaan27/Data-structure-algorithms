6a6fe3dd1f87714ab2dd7c2c322bd61a2a8617d7
Arkaan <arkaankanunga@gmail.com>
2024-02-21 13:10:46 +0000

    implementing top down approach with a recursive solution for minimum cost of climbing stairs

---
+++ Dynamic_programming/min_cost_climb_stairs.py
@@ -0,0 +1,27 @@
+"""
+For a given staircase, the i-th step is assigned a non-negative cost indicated by a cost array.
+
+Once you pay the cost for a step, you can either climb one or two steps. Find the minimum cost to reach the top of the
+staircase. Your first step can either be the first or second step.
+
+"""
+
+
+
+# The top down recursive approach to solving the problem. This is very inefficient but first step
+# to solving the problem
+def min_cost_climbing_stairs_td(cost):
+    n = len(cost)
+    return min(min_cost(n - 1, cost), min_cost(n - 2, cost))
+
+
+def min_cost(i, cost):
+    if i < 0:
+        return 0
+    if i == 0 or i == 1:
+        return cost[i]
+    return cost[i] + min(min_cost(i - 1, cost), min_cost(i - 2, cost))
+
+
+if __name__ == "__main__":
+    print(min_cost_climbing_stairs_td([20, 15, 30, 5]))
